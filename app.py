acd15bc28c2712c6bd5325c401964a4cf7b65dd2
RachhaAshwin <99497220+RachhaAshwin@users.noreply.github.com>
2022-05-08 19:53:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ config = RobertaConfig.from_pretrained('microsoft/codebert-base')
 tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base", do_lower_case  = False)
 
 @st.cache(allow_output_mutation=True)
-def load_model:
+def load_model():
     return model = build_model( model_class = RobertaModel, config = config, tokenizer = tokenizer).to('cpu')
 model = load_model()
 
