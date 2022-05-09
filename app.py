150f73a8fb88f8b515c5d0552a66259c0d02c646
RachhaAshwin <99497220+RachhaAshwin@users.noreply.github.com>
2022-05-09 15:59:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -63,11 +63,10 @@ def build_model(model_class, config, tokenizer):
     )
     return model
 
-config = RobertaConfig.from_pretrained('microsoft/codebert-base')
-tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base", do_lower_case  = False)
-
-@st.cache(hash_funcs={torch.nn.parameter.Parameter: lambda parameter: parameter.data.numpy()})
+@st.cache(allow_output_mutation = True)
 def load_model():
+    config = RobertaConfig.from_pretrained('microsoft/codebert-base')
+    tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base", do_lower_case  = False)
     return build_model( model_class = RobertaModel, config = config, tokenizer = tokenizer).to('cpu')
 model = load_model()
 
